9c65b7a08bf6b2fa61b760318560d60753b20dfc
Nikolaos Agas <agascompany@gmail.com>
2018-05-09 19:21:23 +0300

    init

--- src/webapp/login.java
+++ src/webapp/login.java
@@ -4,6 +4,8 @@ import appLayer.User;
 import sun.rmi.runtime.Log;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -28,12 +30,39 @@ public class login extends HttpServlet {
 
             request.getRequestDispatcher("/welcome.jsp").forward(request,response);
         }else{
+            String DB_url=System.getProperty("JDBC_CONNECTION_STRING");
+            String DB_user=System.getProperty("JDBC_USER");
+            String DB_password=System.getProperty("JDBC_PASSWORD");
             request.setAttribute("errorMsg","Invalid credentials");
             request.getRequestDispatcher("/login.jsp").forward(request,response);
 
         }
 
 
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        System.out.println("firstTime to init()");
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        super.service(req, res);
+        System.out.println("firstTime to serviceServerl()");
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+        System.out.println("firstTime to serviceHttpServerl()");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        System.out.println("firstTime to destroy()");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
